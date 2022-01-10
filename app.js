f4e48299d4ca24a5a798350a830c1fb10040b9d6
Joe Naples <jnaples90@gmail.com>
2022-01-10 18:05:15 -0500

    UI update

--- app.js
+++ app.js
@@ -12,22 +12,24 @@ const overview = document.querySelector(".overview");
 getMovies(API_URL);
 
 function getMovies(url) {
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data.results);
-      showMovies(data.results);
-    });
+   fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+         console.log(data.results);
+         showMovies(data.results);
+      });
 }
 
 function showMovies(data) {
-  main.innerHTML = "";
+   main.innerHTML = "";
 
-  data.forEach((movie) => {
-    const { title, poster_path, vote_average, overview } = movie;
-    const movieEl = document.createElement("div");
-    movieEl.classList.add("movie");
-    movieEl.innerHTML = `
+   data.forEach((movie) => {
+      const { title, poster_path, vote_average, overview, release_date } =
+         movie;
+      const movieEl = document.createElement("div");
+      const year = release_date.split("-");
+      movieEl.classList.add("movie");
+      movieEl.innerHTML = `
       <img
           class='poster'
           src="${IMG_URL + poster_path}"
@@ -35,49 +37,58 @@ function showMovies(data) {
         />
 
         <div class="movie-info">
-            <h3>${title}</h3>
+
             <div class='movie-info__rating'>
-                  <img src='${getApproval(vote_average)}'/> 
-                  <span>${vote_average}/10</span>
+                  <img class='movie-info__star' src='${getApproval(
+                     vote_average
+                  )}'/>
+                  <span>&#160;${vote_average}/10</span>
             </div>
+
+            <h3>${title}</h3>
+
+            <p>Release Year: ${year[0]}</p>
+
         </div>
 
         <div class="overview">
           <h3>Overview</h3>
-          ${overview} 
+          <p>${overview.substring(0, 260)} — <a href="#">View More</a></p>
          </div>
         `;
-    // color changing rating code <span class='${getColor(vote_average)}'>${vote_average}/10</span>;
-    main.appendChild(movieEl);
-  });
+
+      // if overview.length > overview = overview.substring(0, 10)
+      // color changing rating code <span class='${getColor(vote_average)}'>${vote_average}/10</span>;
+      main.appendChild(movieEl);
+   });
 }
 
 function getApproval(rating) {
-  if (rating >= 6) {
-    return "img/thumbs-up.svg";
-  } else {
-    return "img/thumbs-down.svg";
-  }
+   if (rating >= 6) {
+      return "img/star.svg";
+   } else {
+      return "img/star-red.svg";
+   }
 }
 
 function getColor(vote) {
-  if (vote >= 8) {
-    return "green";
-  } else if (vote >= 5) {
-    return "orange";
-  } else {
-    return "red";
-  }
+   if (vote >= 8) {
+      return "green";
+   } else if (vote >= 5) {
+      return "orange";
+   } else {
+      return "red";
+   }
 }
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+   e.preventDefault();
 
-  const searchTerm = search.value;
+   const searchTerm = search.value;
 
-  if (searchTerm) {
-    getMovies(searchURL + "&query=" + searchTerm);
-  } else {
-    getMovies(API_URL);
-  }
+   if (searchTerm) {
+      getMovies(searchURL + "&query=" + searchTerm);
+   } else {
+      getMovies(API_URL);
+   }
 });
